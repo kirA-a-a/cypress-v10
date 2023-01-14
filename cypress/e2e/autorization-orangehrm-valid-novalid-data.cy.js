265d11264ee23f4b0b82d7f3465447d79e2d7d69
KirA-a-a <kiraaaakka@gmail.com>
2023-01-14 16:31:32 +0300

    add one test login page no valid password

--- cypress/e2e/autorization-orangehrm-valid-novalid-data.cy.js
+++ cypress/e2e/autorization-orangehrm-valid-novalid-data.cy.js
@@ -1,12 +1,23 @@
 import {LoginPage} from "./pages/login-page";
 
-const loginPage = new LoginPage()
+const loginPage = new LoginPage();
 
 describe('Все тесты входа в систему', ()=>{
+
+    beforeEach(()=> {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    });
 
     it('Не корректный логин',  () => {
         loginPage.enterUserName('Admin123');
         loginPage.enterPassword('admin123');
+        loginPage.clickLogin();
+        loginPage.checkError('Invalid credentials');
+    });
+
+    it('Не корректный пароль',  () => {
+        loginPage.enterUserName('Admin');
+        loginPage.enterPassword('admin');
         loginPage.clickLogin();
         loginPage.checkError('Invalid credentials');
     });
@@ -17,7 +28,4 @@ describe('Все тесты входа в систему', ()=>{
         loginPage.clickLogin();
         loginPage.checkEnterProfile();
     });
-})
-
-
-
+});
